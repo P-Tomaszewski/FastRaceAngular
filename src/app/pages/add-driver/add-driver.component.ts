ca137f3c0d1ef7ba5f8d0a729608164c549485d5
Patryk <ptomaszewskig3@gmail.com>
2021-01-21 23:14:05 +0100

    s2

--- src/app/pages/add-driver/add-driver.component.ts
+++ src/app/pages/add-driver/add-driver.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {Driver} from '../../spec/driver';
+import {ActivatedRoute, Router} from '@angular/router';
+import {DriverService} from '../../service/driver.service';
 
 @Component({
   selector: 'app-add-driver',
   templateUrl: './add-driver.component.html',
   styleUrls: ['./add-driver.component.css']
 })
-export class AddDriverComponent implements OnInit {
+export class AddDriverComponent {
 
-  constructor() { }
+  driver: Driver;
 
-  ngOnInit(): void {
+  constructor( private route: ActivatedRoute,
+               private router: Router,
+               private driverService: DriverService) {
+    this.driver = new Driver();
+  }
+
+  onSubmit() {
+    this.driverService.save(this.driver).subscribe(result => this.gotoUserList());
+  }
+
+  gotoUserList() {
+    this.router.navigate(['/top']);
   }
 
 }
